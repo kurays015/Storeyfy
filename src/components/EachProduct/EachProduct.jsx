847ee1d5145f33e9dc5ecd6c3fec7f91b2a18c83
Christzxz <christnarvarte15@gmail.com>
2023-09-29 22:32:02 +0800

    fix ui on EachProduct component

--- src/components/EachProduct/EachProduct.jsx
+++ src/components/EachProduct/EachProduct.jsx
@@ -1,15 +1,14 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDataFetching } from "../../FetchProductsDetails";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { FreeMode } from "swiper/modules";
 import imgPlaceholder from "/images/placeholder.jpg";
 
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-
 function Products() {
+  //set the default/main img to placeholder
+  const [currentImage, setCurrentImage] = useState(imgPlaceholder);
+  const [cartQuantity, setCartQuantity] = useState(1);
+  const [addPrice, setAddPrice] = useState(null);
+
   const { id } = useParams();
   const fetchEachProductById = () => {
     return useDataFetching(
@@ -19,9 +18,6 @@ function Products() {
   };
   const { data } = fetchEachProductById();
 
-  //set the default/main img to placeholder
-  const [currentImage, setCurrentImage] = useState(imgPlaceholder);
-
   useEffect(() => {
     if (data) {
       setCurrentImage(data.thumbnail);
@@ -30,63 +26,74 @@ function Products() {
 
   function handleImageHoverAndClick(img) {
     setCurrentImage(img);
+    setBorder(true);
   }
 
+  function addQuantity() {
+    setCartQuantity(prev => prev + 1);
+    setAddPrice(prev => ({ ...prev }));
+  }
+  function minusQuantity() {
+    if (cartQuantity > 1) {
+      setCartQuantity(prev => prev - 1);
+    }
+  }
   return (
-    <div className="each-productContainer">
-      <div>
-        <div className="main-img-container">
-          <img src={currentImage} alt={data?.title} className="main-img" />
-        </div>
-        <div className="swiper-container">
-          <Swiper
-            slidesPerView={5}
-            spaceBetween={30}
-            freeMode={true}
-            modules={[FreeMode]}
-            className="swiper"
-          >
-            {data?.images.map((image, index) => (
-              <SwiperSlide className="each-slide-container">
-                <img
-                  key={index}
-                  src={image}
-                  alt={data?.title}
-                  className="each-slide-img"
-                  onMouseEnter={() => handleImageHoverAndClick(image)}
-                  onClick={() => handleImageHoverAndClick(image)}
-                />
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
-      </div>
-      <div className="product-specs">
-        <div>Brand: {data?.brand}</div>
-        <div>
-          Stock:
-          <span className={data?.stock <= 10 ? "out-of-stock" : "in-stock"}>
-            {data?.stock <= 10 ? `Only ${data?.stock} stock left` : data?.stock}
-          </span>
-        </div>
-        <h1>{data?.title}</h1>
-        <div>Rating: {data?.rating}</div>
-        <div>
-          <div>Description:</div>
-          <p>{data?.description}</p>
-        </div>
-        <div className="each-price">${data?.price}</div>
-        <div className="cart-and-buy">
-          <div className="cart-count">
-            <button>-</button>
-            <div>1</div>
-            <button>+</button>
+    <>
+      <div className="each-productContainer">
+        <div className="images-container">
+          <div className="main-img-container">
+            <img src={currentImage} alt={data?.title} className="main-img" />
           </div>
-          <button>Buy Now</button>
-          <button>Add to Cart</button>
+          <div className="swiper-container">
+            {data?.images.map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt={data?.title}
+                className="each-slide-img"
+                onMouseEnter={() => handleImageHoverAndClick(image)}
+                onClick={() => handleImageHoverAndClick(image)}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="product-specs">
+          <div>Brand: {data?.brand}</div>
+          <div>Category: {data?.category}</div>
+          <p>
+            Stock:{" "}
+            <span className={data?.stock <= 10 ? "out-of-stock" : "in-stock"}>
+              {data?.stock}
+            </span>{" "}
+            left
+          </p>
+          <h2>{data?.title.toUpperCase()}</h2>
+          <div>Rating: {data?.rating} need to fix this, turn to star</div>
+          <div>
+            <div>Description:</div>
+            <p>{data?.description}</p>
+          </div>
+          <div className="quantity-container">
+            <div>Quantity</div>
+            <div className="cart-count-container">
+              <button className="quantity-minus" onClick={minusQuantity}>
+                -
+              </button>
+              <div className="cart-quantity">{cartQuantity}</div>
+              <button className="quantity-add" onClick={addQuantity}>
+                +
+              </button>
+            </div>
+            <div className="each-price">${data?.price}</div>
+          </div>
+          <div className="cartbuy-btn">
+            <button className="buynow-btn">Buy Now</button>
+            <button className="addtocart-btn">Add to Cart</button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
