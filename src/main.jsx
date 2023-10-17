49af3e70cff5db557a384405e1c457ff2127d1c8
Christzxz <christnarvarte15@gmail.com>
2023-10-17 15:58:19 +0800

    fix import

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 //react query
 import { QueryClientProvider, QueryClient } from "react-query";
 //context
-import { WishistProvider } from "./Context/WishListContext.jsx";
+import { WishlistProvider } from "./Context/WishlistContext.jsx";
 import { CartProvider } from "./Context/CartContext.jsx";
 
 //components
