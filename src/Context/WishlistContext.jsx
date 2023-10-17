79dca9e7ae59c1a4faa69febaf7a75c17c776881
Christzxz <christnarvarte15@gmail.com>
2023-10-17 14:51:58 +0800

    fix

--- src/Context/WishlistContext.jsx
+++ src/Context/WishlistContext.jsx
@@ -30,25 +30,24 @@ export function WishlistProvider({ children }) {
     //data is getting undefined on the first load, need to make sure it's true
     if (productData) {
       //check if the product is already in the wishlist when product clicked id is matched
-      const isAlreadyInTheWishList = wishListItems.some(
-        item => item.id === productData.id
-      );
-
-      //if it's not in the wishlist, add it
-      if (!isAlreadyInTheWishList) {
-        setIsHeartFilled(true);
-        setAlreadyInTheWishList(false);
-        setShowWishlistMessage(true);
-        setWishListItems([...wishListItems, productData]);
-      } else {
-        //if it's in the wishlist, remove it using filter
-        setAlreadyInTheWishList(true);
-        setIsHeartFilled(false);
-        const updatedWishlist = wishListItems.filter(
-          item => item.id !== productData.id
-        );
-        setWishListItems(updatedWishlist);
-      }
+      // const isAlreadyInTheWishList = wishListItems.some(
+      //   item => item.id === productData.id
+      // );
+      // //if it's not in the wishlist, add it
+      // if (!isAlreadyInTheWishList) {
+      //   setIsHeartFilled(true);
+      //   setAlreadyInTheWishList(false);
+      //   setShowWishlistMessage(true);
+      //   setWishListItems([...wishListItems, productData]);
+      // } else {
+      //   //if it's in the wishlist, remove it using filter
+      //   setAlreadyInTheWishList(true);
+      //   setIsHeartFilled(false);
+      //   const updatedWishlist = wishListItems.filter(
+      //     item => item.id !== productData.id
+      //   );
+      //   setWishListItems(updatedWishlist);
+      // }
     }
     setTimeout(() => setShowMessageContainer(false), 4000);
   }
